9d707e9f8493193563ca8b760a4ffb4580cd3137
root <root@rezendeneto-its-nb.(none)>
2012-06-10 20:48:12 -0300

    funfa

--- main.cpp
+++ main.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
+#define BUFF_SIZE 3
+
 int main() {
 
 	int fd1;
-	char* buff = new char[3];
+	char* buff = new char[3*BUFF_SIZE];
 	int wr;
 
 	fd1 = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
@@ -18,11 +20,11 @@ int main() {
 		if (fd1 == -1)
 			cout << "Error connnecting" << endl;
 		else {
-			wr = write(fd1, "abc", 3);
+			wr = write(fd1, "000", BUFF_SIZE);
 
 			int bytes = 0;
 
-            while(bytes = read(fd1, buff, 9) > 0) {
+            while(bytes = read(fd1, buff, 3*BUFF_SIZE) > 0) {
                 cout << buff << endl;
             }
 		}
